48843e5c8398e0beffbbd926c18ae95b56a0b0c7
Artem Petrov <Artyom1350@gmail.com>
2024-10-19 10:24:32 +0500

    feat: Реализация функционала

---
+++ apps/dynamic-echarts/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'dynamic-echarts',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/dynamic-echarts',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
